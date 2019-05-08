e2c00833d862010e62205cd55532a7308920058d
Niranjan Shankar <nshankar@princeton.edu>
2019-05-08 19:46:21 -0400

    fixed

--- pages/myEvents.js
+++ pages/myEvents.js
@@ -249,21 +249,21 @@ class eventList extends React.Component {
             "Content-Type": "application/json"
         }
     });
-    const res2 = await fetch(database_url + '/visitor_pairing/delete_events/' + this.state.current_pairing.event_id, {
+    const res2 = await fetch(database_url + '/visitor_pairing/delete_events/' + this.state.current_event.event_id, {
         method: "DELETE",
         headers: {
             'Accept': 'application/json',
             "Content-Type": "application/json"
         }
     });
-    const res3 = await fetch(database_url + '/pairing/delete_events/' + this.state.current_pairing.event_id, {
+    const res3 = await fetch(database_url + '/pairing/delete_events/' + this.state.current_event.event_id, {
         method: "DELETE",
         headers: {
             'Accept': 'application/json',
             "Content-Type": "application/json"
         }
     });
-    const res4 = await fetch(database_url + '/eligibility/delete_events/' + this.state.current_pairing.event_id, {
+    const res4 = await fetch(database_url + '/eligibility/delete_events/' + this.state.current_event.event_id, {
         method: "DELETE",
         headers: {
             'Accept': 'application/json',
